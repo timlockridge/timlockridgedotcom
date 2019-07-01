f602593f14c20cd5e42c9a9a4d5b323f8d6851ba
Tim Lockridge <timlockridge@gmail.com>
2019-07-01 17:16:05 -0400

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,5 +4,5 @@ if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
   export SSHPASS=$PASSWORD
   cd _site || exit
-  sshpass -e scp -r . $USERNAME@$HOST:/home/timlockridge/mainwebsite_html
+  sshpass -e scp -o StrictHostKeyChecking=no -r . $USERNAME@$HOST:/home/timlockridge/mainwebsite_html
 fi
