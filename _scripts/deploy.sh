ff6610d5e65258349166dec24e0c13b4b843a957
Tim Lockridge <timlockridge@gmail.com>
2019-07-01 17:09:15 -0400

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-SSHPASS=$PASSWORD
-cd _site || exit
-sshpass -e scp -r . $USERNAME@$HOST:/home/timlockridge/mainwebsite_html
+if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+then
+  SSHPASS=$PASSWORD
+  cd _site || exit
+  sshpass -e scp -r . $USERNAME@$HOST:/home/timlockridge/mainwebsite_html
+fi
