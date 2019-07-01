63f3484ff686a28c4541332581c1f405f9c127a4
Tim Lockridge <timlockridge@gmail.com>
2019-07-01 16:42:00 -0400

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-EOF
+cd _site || exit
 
-    cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /home/timlockridge/mainwebsite_html .
-fi
+sshpass -e scp -r . $USERNAME@$HOST:/home/timlockridge/mainwebsite_html
